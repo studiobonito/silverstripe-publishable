6058bae44952f74b93850ffb94b859356e3438e7
Tom Densham <tom.densham@studiobonito.co.uk>
2013-11-04 10:58:44 +0000

    ADD: Display stage related status badges

--- code/forms/gridfield/PublishableGridFieldConfig.php
+++ code/forms/gridfield/PublishableGridFieldConfig.php
@@ -33,13 +33,37 @@ class PublishableGridFieldConfig_Base extends PublishableGridFieldConfig
         $this->addComponent(new GridFieldToolbarHeader());
         $this->addComponent($sort = new GridFieldSortableHeader());
         $this->addComponent($filter = new GridFieldFilterHeader());
-        $this->addComponent(new GridFieldDataColumns());
+        $this->addComponent($columns = new GridFieldDataColumns());
         $this->addComponent(new GridFieldPageCount('toolbar-header-right'));
         $this->addComponent($pagination = new GridFieldPaginator($itemsPerPage));
 
         $sort->setThrowExceptionOnBadDataType(false);
         $filter->setThrowExceptionOnBadDataType(false);
         $pagination->setThrowExceptionOnBadDataType(false);
+
+        $columns->setFieldFormatting(array(
+            'Title' => function($value, &$item) {
+                $badge = array();
+                if ($item->ExistsOnLive && $item->IsModifiedOnStage) {
+                    $badge['class'] = 'modified';
+                    $badge['title'] = _t('PublishableGridFieldStatusColumns.ModifiedStage', 'Modified');
+                } elseif ($item->IsAddedToStage) {
+                    $badge['class'] = 'addedtodraft';
+                    $badge['title'] = _t('PublishableGridFieldStatusColumns.Stage', 'Draft');
+                }
+
+                $return = $item->Title;
+                if(isset($badge['class']) && isset($badge['title'])) {
+                    $return .= sprintf(
+                        "<span class=\"badge %s\">%s</span>",
+                        'status-' . Convert::raw2xml($badge['class']),
+                        Convert::raw2xml($badge['title'])
+                    );
+                }
+
+                return $return;
+            }
+        ));
     }
 }
 
@@ -65,7 +89,7 @@ class PublishableGridFieldConfig_RecordViewer extends PublishableGridFieldConfig
         $this->addComponent(new GridFieldToolbarHeader());
         $this->addComponent($sort = new GridFieldSortableHeader());
         $this->addComponent($filter = new GridFieldFilterHeader());
-        $this->addComponent(new GridFieldDataColumns());
+        $this->addComponent($columns = new GridFieldDataColumns());
         $this->addComponent(new PublishableGridFieldViewButton());
         $this->addComponent(new GridFieldPageCount('toolbar-header-right'));
         $this->addComponent($pagination = new GridFieldPaginator($itemsPerPage));
@@ -74,6 +98,30 @@ class PublishableGridFieldConfig_RecordViewer extends PublishableGridFieldConfig
         $sort->setThrowExceptionOnBadDataType(false);
         $filter->setThrowExceptionOnBadDataType(false);
         $pagination->setThrowExceptionOnBadDataType(false);
+
+        $columns->setFieldFormatting(array(
+            'Title' => function($value, &$item) {
+                $badge = array();
+                if ($item->ExistsOnLive && $item->IsModifiedOnStage) {
+                    $badge['class'] = 'modified';
+                    $badge['title'] = _t('PublishableGridFieldStatusColumns.ModifiedStage', 'Modified');
+                } elseif ($item->IsAddedToStage) {
+                    $badge['class'] = 'addedtodraft';
+                    $badge['title'] = _t('PublishableGridFieldStatusColumns.Stage', 'Draft');
+                }
+
+                $return = $item->Title;
+                if(isset($badge['class']) && isset($badge['title'])) {
+                    $return .= sprintf(
+                        "<span class=\"badge %s\">%s</span>",
+                        'status-' . Convert::raw2xml($badge['class']),
+                        Convert::raw2xml($badge['title'])
+                    );
+                }
+
+                return $return;
+            }
+        ));
     }
 }
 
@@ -99,7 +147,7 @@ class PublishableGridFieldConfig_RecordEditor extends PublishableGridFieldConfig
         $this->addComponent(new GridFieldToolbarHeader());
         $this->addComponent($sort = new GridFieldSortableHeader());
         $this->addComponent($filter = new GridFieldFilterHeader());
-        $this->addComponent(new GridFieldDataColumns());
+        $this->addComponent($columns = new GridFieldDataColumns());
         $this->addComponent(new PublishableGridFieldDeleteAction());
         $this->addComponent(new PublishableGridFieldPublishAction());
         $this->addComponent(new PublishableGridFieldEditButton());
@@ -110,5 +158,29 @@ class PublishableGridFieldConfig_RecordEditor extends PublishableGridFieldConfig
         $sort->setThrowExceptionOnBadDataType(false);
         $filter->setThrowExceptionOnBadDataType(false);
         $pagination->setThrowExceptionOnBadDataType(false);
+
+        $columns->setFieldFormatting(array(
+            'Title' => function($value, &$item) {
+                $badge = array();
+                if ($item->ExistsOnLive && $item->IsModifiedOnStage) {
+                    $badge['class'] = 'modified';
+                    $badge['title'] = _t('PublishableGridFieldStatusColumns.ModifiedStage', 'Modified');
+                } elseif ($item->IsAddedToStage) {
+                    $badge['class'] = 'addedtodraft';
+                    $badge['title'] = _t('PublishableGridFieldStatusColumns.Stage', 'Draft');
+                }
+
+                $return = $item->Title;
+                if(isset($badge['class']) && isset($badge['title'])) {
+                    $return .= sprintf(
+                        "<span class=\"badge %s\">%s</span>",
+                        'status-' . Convert::raw2xml($badge['class']),
+                        Convert::raw2xml($badge['title'])
+                    );
+                }
+
+                return $return;
+            }
+        ));
     }
 }
